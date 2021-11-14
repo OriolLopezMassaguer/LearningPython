a51f3e1d1e6b4d3bc2451b45169650d474165522
Oriol Lopez Massaguer <oriol.lopez@gmail.com>
2021-11-14 10:05:04 +0100

    data structures

--- data_structures.py
+++ data_structures.py
@@ -1,3 +1,10 @@
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 
 fruits.count('apple')
+
+frutas = {'naranja','mandarina','melocotón','pera','manzana'}
+
+a = set('abracadabra')
+b = set('alacazam')
+
+tel = {'jack':4098, 'sape':4139}
